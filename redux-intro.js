99b6b11f59bfc5ddacae9b30e1f729e357a58061
Am1d1v <dimadiman1adima@gmail.com>
2024-01-21 18:04:14 +0300

    Redux Intro

--- redux-intro.js
+++ redux-intro.js
@@ -2,10 +2,24 @@ const redux = require('redux');
 
 
 // Reducer Function
-const reducerCounter = (state, action) => {
-    return {
-        counter: state.counter + 1,
-    };
+const reducerCounter = (state = {counter: 0}, action) => {
+
+    // Increase Counter by 1
+    if(action.type === "increment"){
+        return {
+            counter: state.counter + 1,
+        };
+    } 
+    
+    // Decrease Counter by 1
+    if (action.type === "decrement"){
+        return {
+            counter: state.counter - 1,
+        };
+    }
+
+    return state;
+    
 };
 
 // Store Creation
@@ -13,9 +27,14 @@ const store =  redux.createStore(reducerCounter);
 
 // Subscribe for Counter
 const counterSubscribe = () => {
-    console.log(store.getState());
+    const newState = store.getState();
+    console.log(newState);
 }
 
 store.subscribe(counterSubscribe)
 
-counterSubscribe();
+// Action type "increment"
+store.dispatch({type: "increment"})
+
+// Action type "decrement"
+store.dispatch({type: "decrement"})
